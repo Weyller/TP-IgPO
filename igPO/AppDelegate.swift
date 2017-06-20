3373a6838a3af22e149251e05207ac89dc7a8ad3
eleves <eleves@mac404inf-09.local>
2017-06-20 15:21:55 -0400

    pause 2 sec

--- igPO/AppDelegate.swift
+++ igPO/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        //--------------------------------- Pause de 2 secondes avant de passer a la 1ere page
+        sleep(2)
+        
         return true
     }
 
