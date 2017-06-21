1e2752eddef2981fca66176a5dde76ba44b49a95
eleves <eleves@mac404inf-09.local>
2017-06-21 16:59:00 -0400

    new login controller

---
+++ LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  igPO
+//
+//  Created by eleves on 2017-06-21.
+//  Copyright © 2017 Mario Geneau. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
